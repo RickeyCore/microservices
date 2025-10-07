03ff75d9daec78257fb4f7fa33c90c3e3691b8cb
RickeyCore <kotovski.nichita@mail.ru>
2025-10-07 17:49:28 +0400

    Added RestTemplate

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -2,18 +2,29 @@ package com.amigoscode.customer;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
-
+    private final RestTemplate restTemplate;
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        var fraudResponse = restTemplate.getForObject(
+                "http://localhost:8082/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
     }
 }
