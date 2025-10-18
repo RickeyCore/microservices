ef3b6e05512a0a740da39a0bfd8853c7c43a5601
RickeyCore <kotovski.nichita@mail.ru>
2025-10-18 09:51:42 +0400

    refactoring services for k8s

--- customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 
 @SpringBootApplication(
@@ -15,6 +17,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients(
         basePackages = "com.amigoscode.clients"
+)
+@PropertySources(
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
 )
 public class CustomerApplication
 {
